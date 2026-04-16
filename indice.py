035411860a7e974ea5f810eb8d94b057b0621b6d
Alan222-coder <alanfenix222@gmail.com>
2026-04-15 22:48:43 -0300

    3 circulos
    Estudiando un poco las funciones y parámetros de estas se agrego una lista que contiene listas para poder usar estas listas para graficar circulos

--- indice.py
+++ indice.py
@@ -3,6 +3,14 @@ from OpenGL.GLUT import *
 from OpenGL.GLU import *
 import math
 
+circulos = [
+    (1.0, 0.5, 0.0, 0.0),  # Color azul, tamaño 0.5, posición (0.0, 0.0)
+    (1.0, 0.2, -0.5, -0.5), # Color azul, tamaño 0.2, posición (-0.5, -0.5)
+    (1.0, 0.2, 0.5, -0.5)   # Color azul, tamaño 0.2, posición (0.5, -0.5)
+]
+
+
+
 def draw_circle(x, y, radius, segments=100): # Funcion de circulo, no borrar
 
     glBegin(GL_TRIANGLE_FAN)
@@ -16,10 +24,13 @@ def draw_circle(x, y, radius, segments=100): # Funcion de circulo, no borrar
 
 
 def display():
+
+
     glClear(GL_COLOR_BUFFER_BIT)
-    
-    glColor3f(0.0, 0.0, 1.0)  # Color rojo
-    draw_circle(0.0, 0.0, 0.5)
+
+    for Color, tamaño, x, y in circulos:
+        glColor3f(0.0, 0.0, Color)
+        draw_circle(x, y, tamaño)
 
     glFlush()
 
@@ -38,8 +49,10 @@ def main():
     glutCreateWindow("Círculo en PyOpenGL".encode("utf-8"))
 
     init()
+
     glutDisplayFunc(display)
     glutMainLoop()
 
 if __name__ == "__main__":
     main()
+
